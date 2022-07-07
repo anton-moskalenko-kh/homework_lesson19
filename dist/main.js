f215f1f6336fa3e09377be440193747f95ae4d66
anton-moskalenko-kh <anton.moskalenko.kh@gmail.com>
2022-07-07 19:17:56 +0300

    Homework_ver.1.5.0

--- dist/main.js
+++ dist/main.js
@@ -1,6 +1,12 @@
 "use strict";
 
-function checkArray() {
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); enumerableOnly && (symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; })), keys.push.apply(keys, symbols); } return keys; }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = null != arguments[i] ? arguments[i] : {}; i % 2 ? ownKeys(Object(source), !0).forEach(function (key) { _defineProperty(target, key, source[key]); }) : Object.getOwnPropertyDescriptors ? Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)) : ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+var checkArray = function checkArray() {
   var a = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'Error';
   var b = arguments.length > 1 ? arguments[1] : undefined;
 
@@ -8,14 +14,10 @@ function checkArray() {
     rest[_key - 2] = arguments[_key];
   }
 
-  var map = new Map();
-  arguments.length < 5 ? console.log("Error") : console.log("".concat(a, " - ").concat(b, " - ").concat(rest[0]));
-  var args = Array.from(arguments).slice(3);
-  var obj = args.map(function (item) {
-    var index = args.indexOf(item);
-    map.set(index, item);
-  });
-  console.log(Object.fromEntries(map));
-}
+  var arr = [a, b].concat(rest);
+  arr.length < 5 ? console.log("Error") : console.log("".concat(a, " - ").concat(b, " - ").concat(rest[0]));
+  var args = arr.slice(3);
+  console.log(_objectSpread({}, args));
+};
 
 checkArray(undefined, 2, 3, 4, 5, 'sd');
