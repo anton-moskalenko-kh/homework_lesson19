bded750eaa3837e68a1e8091ef7c1f16be5b6bb7
anton-moskalenko-kh <anton.moskalenko.kh@gmail.com>
2022-07-07 16:50:42 +0300

    Homework_ver.1.3.0

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ function checkArray(a = 'Error', b, ...rest) {
     (arguments.length < 5) ? console.log("Error") : console.log(`${a} - ${b} - ${rest[0]}`)
 
     const args = Array.from(arguments).slice(3);
-    const mapValues = args.map((item) => {
+    args.forEach((item) => {
         const index = args.indexOf(item)
         map.set(index, item)
     })
