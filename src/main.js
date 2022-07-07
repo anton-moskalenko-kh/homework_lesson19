83975883dcc63838d7bf70433913e7efa42e6e10
anton-moskalenko-kh <anton.moskalenko.kh@gmail.com>
2022-07-07 19:03:03 +0300

    Homework_ver.1.4.0

--- src/main.js
+++ src/main.js
@@ -1,13 +1,10 @@
-function checkArray(a = 'Error', b, ...rest) {
-    const map = new Map;
-    (arguments.length < 5) ? console.log("Error") : console.log(`${a} - ${b} - ${rest[0]}`)
+let checkArray = (a = 'Error', b, ...rest) => {
+    const arr = [a,b, ...rest];
+    (arr.length < 5) ? console.log("Error") : console.log(`${a} - ${b} - ${rest[0]}`)
 
-    const args = Array.from(arguments).slice(3);
-    args.forEach((item) => {
-        const index = args.indexOf(item)
-        map.set(index, item)
-    })
-    console.log(Object.fromEntries(map))
+    const args = arr.slice(3);
+    console.log({...args})
 }
 
 checkArray(undefined,2,3,4,5,'sd')
+
